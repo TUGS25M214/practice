616f8c6d3c173c4802771efe7224b4c076c4887d
Koki Hinaiji <25m214@st.takushoku-u.ac.jp>
2025-05-22 11:03:45 +0900

    第5回

--- cv_test.cpp
+++ cv_test.cpp
@@ -2,22 +2,39 @@
 #include <opencv2/opencv.hpp>
 
 int main(){
-    cv::Mat image0 = cv::imread("barbara.ppm", cv::IMREAD_COLOR_RGB);
+    cv::Mat image0 = cv::imread("barbara.ppm", cv::IMREAD_COLOR_BGR);
 
     const int w = image0.cols; // 横
     const int h = image0.rows; // 縦
     const int nc = image0.channels();
+    const int XR = 32;
+    const int YR = 32; 
+
+
+    /*************** Encoder ***************/
+    // 色空間返還: RGB -> YCbCr
+    cv::cvtColor(image0, image0, cv::COLOR_BGR2YCrCb);
     auto *p = image0.data; // address of top-left pixel
 
-    for (int y = 0; y < h; ++y){
-        for (int x = 0; x < w; ++x){
-            for (int c = 0; c < nc; ++c){
-                p[nc * (y * w + x) + c] /= 2;//課題RGBで読み込んだ値をBGRに並べ替える
-            }
-        }
-    }
+    std::vector<cv::Mat> ycrcb;
+    cv::split(image0, ycrcb);// Y, Cr ,Cbをそれぞれ独立したMatに分割
 
-    cv::imshow("imshow window", image0);
+    // 色間引き処理
+    cv::resize(ycrcb[1], ycrcb[1], cv::Size(), 1.0 / XR, 1.0 / YR, cv::INTER_LINEAR);
+    cv::resize(ycrcb[2], ycrcb[2], cv::Size(), 1.0 / XR, 1.0 / YR, cv::INTER_LINEAR);
+
+
+    /*************** Decoder ***************/
+    //色差成分を補完
+    cv::resize(ycrcb[1], ycrcb[1], cv::Size(), XR, YR, cv::INTER_LINEAR);
+    cv::resize(ycrcb[2], ycrcb[2], cv::Size(), XR, YR, cv::INTER_LINEAR);
+    cv::merge(ycrcb, image0);
+    
+
+    // 色空間返還: YCbCr -> RGB
+    cv::cvtColor(image0, image0, cv::COLOR_YCrCb2BGR);
+
+    cv::imshow("image0", image0);
     cv::waitKey(0);
     return 0;
 }
